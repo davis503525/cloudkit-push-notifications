88c30211031066a34fdeffbb4ccf5f95efd8a829
Davis Allie <davis525@live.com.au>
2017-01-28 15:29:54 +1100

    Initial Commit

---
+++ CloudKit-Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudKit-Notifications
+//
+//  Created by Davis Allie on 28/01/16.
+//  Copyright © 2017 Davis Allie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
